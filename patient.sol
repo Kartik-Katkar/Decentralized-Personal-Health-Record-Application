beff3e0e5933bab7646e1ae8e5f9678d41e715df
Subham Jyoti <86204779+Derixtar54@users.noreply.github.com>
2024-05-05 20:21:13 +0530

    Update patient.sol

--- patient.sol
+++ patient.sol
@@ -1,8 +1,5 @@
 pragma solidity >=0.4.22 <0.7.0;
-/**
- * @title Medical records
- * @dev Store & retreive patient details 
- */
+
 
  
 contract Patient {
@@ -42,7 +39,7 @@ contract Patient {
       }
       
       
-      // modifier to give access only to hospital
+     
       modifier isOwner() {
 
          require(msg.sender == owner, "Access is not allowed");
@@ -54,19 +51,7 @@ contract Patient {
      
      
      
-     /**
-     * @dev Store patient details
-     * @param patient_id patient id
-     * @param _patient_name patient name
-     * @param _age age
-     * @param _gender gender
-     * @param _height height
-     * @param _weight weight
-     * @param _patient_address address
-     * @param _phone_no phone number
-     * @param _email_id mail id
-     * @param _date date
-     */
+   
      function store_patient_details(uint256 patient_id,string memory _patient_name,uint256 _age,string memory _gender,string memory _height,uint256 _weight,string memory _patient_address,uint256 _phone_no,string memory _email_id,uint256 _date)public isOwner {
      
         
@@ -84,13 +69,7 @@ contract Patient {
          }
          
          
-     /**
-     * @dev Store attendant details
-     * @param patient_id patient id
-     * @param _attendant_name name of attendant
-     * @param _attendant_relation relation to patient
-     * @param _attendant_phn_no phone no
-     * */
+    
      function store_attendant_details(uint256 patient_id,string memory _attendant_name,string memory _attendant_relation, uint256 _attendant_phn_no)public isOwner {
          
          a.patient_id = patient_id;
@@ -102,10 +81,7 @@ contract Patient {
      }
      
      
-     /**
-     * @dev Retreive patient details
-     * @param patient_id patient id
-     * */
+    
      function retreive_patient_details(uint256 patient_id) public view returns (string memory,uint256,string memory,string memory,uint256,string memory,uint256,string memory,uint256){
          patient memory p = patientlist[patient_id];
          
@@ -114,10 +90,7 @@ contract Patient {
      }
      
      
-     /**
-     * @dev Retreive attendant details
-     * @param patient_id patient id
-     * */
+    
      function retreive_attendant_details(uint256 patient_id) public view returns (string memory,string memory,uint256){
          
           attendant memory a = attendantlist[patient_id];
